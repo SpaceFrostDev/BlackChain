5b47a7443689eaf355656063b46250473cc4ac87
sfdev <72049279+SpaceFrostDev@users.noreply.github.com>
2022-03-29 19:23:40 -0700

    Scrapped Chainlink VRF

---
+++ roulette.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.5.0;
+
+/*
+@dev Betting Strategy Codes Table
+    0: Player's strategy is betting on an exact number
+    1: Even/odd
+    2: Color
+    3. High/Low
+
+@dev Subcodes
+    0: Subcode is exact number
+    1: 0 is Even, 1 is Odd
+    2. 0 is Red, 1 is Black
+    3. 0 is Low, 1 is High
+
+*/
+
+
+contract Roulette {
+    uint8 state;    // This state of the roulette board
+    uint pot;       // Total pot
+
+    struct Bet{
+        uint256 amount;    // Amount of bet in wei
+        address player;
+        uint8 betCode;     // See betting strategy codes table
+        uint8 subCode;     // See subcodes
+    }
+
+    Bet[] public currentBets;    // Publicly viewable array holding all current player bets
+
+}
