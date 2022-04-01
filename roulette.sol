0f5dda6155e4dc839f61775e5769702c1a479e88
sfdev <72049279+SpaceFrostDev@users.noreply.github.com>
2022-03-31 17:58:18 -0700

    Bug fixes, code styling

--- roulette.sol
+++ roulette.sol
@@ -21,7 +21,7 @@ contract Roulette {
     using SafeMath for uint256;
     uint8[] red = [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36];
     uint8[] black = [2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35];
-    uint256 houseBalance = 0;
+    uint256 houseBalance = 100_000_000_000_000_000;
 
     struct Bet{
         uint256 amount;    // Amount of bet in wei
@@ -49,39 +49,30 @@ contract Roulette {
             Bet memory bet = currentBets[i];
 
             // Switch statement from C++ would be ideal
-            if (randNum == 37 || randNum == 38){    // Player loses regardless of bet strat
-                break;
-            }
-            if (bet.betCode == 0) {
-                isWin = (randNum == bet.subCode);
-            }
-            if (bet.betCode == 1) {
-                isWin = (bet.subCode == (randNum % 2));
-            }
-            if (bet.betCode == 2) {
-                if (bet.subCode == 0) {    // bet on black
-                    if (randNum <= 10 || (randNum>= 20 && randNum <= 28)) {
-                        isWin = (randNum % 2 == 0);    // If in range, must be even
+            if (randNum != 0 && randNum != 37) {    // Player loses regardless of bet strat
+                if (bet.betCode == 0) {
+                    isWin = (randNum == bet.subCode); }
+                if (bet.betCode == 1) {
+                    isWin = (bet.subCode == (randNum % 2)); }
+                if (bet.betCode == 2) {
+                    if (bet.subCode == 0) {    // bet on black
+                        if (randNum <= 10 || (randNum>= 20 && randNum <= 28)) {
+                            isWin = (randNum % 2 == 0); }   // If in range, must be even
+                        else {
+                            isWin = (randNum % 2 == 1); }   // If not in that range, must be odd
                     }
+                    else {    // bet on red
+                        if ((randNum >= 12 && randNum <= 18) || randNum >= 30) {
+                            isWin = (randNum % 2 == 0); }
+                        else {
+                            isWin = (randNum % 2 == 1); }
+                    }
+                }
+                if (bet.betCode == 3) {    // bet high/low
+                    if (bet.subCode == 0) {
+                        isWin = (randNum < 16); }
                     else {
-                        isWin = (randNum % 2 == 1);    // If not in that range, must be odd
-                    }
-                }
-                else {    // bet on red
-                    if ((randNum >= 12 && randNum <= 18) || randNum >= 30) {
-                        isWin = (randNum % 2 == 0);
-                    }
-                    else {
-                        isWin = (randNum % 2 == 1);
-                    }
-                }
-            }
-            if (bet.betCode == 3) {    // bet high/low
-                if (bet.subCode == 0) {
-                    isWin = (randNum < 16);
-                }
-                else {
-                    isWin = (randNum >= 16);
+                        isWin = (randNum >= 16); }
                 }
             }
 
@@ -90,8 +81,7 @@ contract Roulette {
                 bet.player.transfer(bet.amount);
             }
             else {
-                houseBalance.add(bet.amount);
-            }
+                houseBalance.add(bet.amount); }
         }
 
         // @dev Delete bets
